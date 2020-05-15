32213d408ab2e309d2a8012678e7bcb1249361c6
Brett Andrews <brettandrews3@gmail.com>
2020-05-15 10:40:11 -0500

    Testing

---
+++ IntermittentFastingApp/IntermittentFastingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IntermittentFastingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
