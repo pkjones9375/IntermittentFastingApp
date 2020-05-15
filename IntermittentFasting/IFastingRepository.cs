50a5c9089af91d2680e0d53bd42d423fe9acd0ae
Brett Andrews <brettandrews3@gmail.com>
2020-05-15 13:13:35 -0500

    IFastingRepo done

---
+++ IntermittentFasting/IFastingRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace IntermittentFasting
+{
+    //Here's the layout for our IF user info. Console will get/set BMI info
+    //UserID will act as Primary Key in SQL
+    public class IFastingRepository
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int HeightInches { get; set; }
+        public decimal StartBMI { get; set }
+        public decimal LastBMI { get; set; }
+        public int FastingHours { get; set; }
+    }
+}
