383f91c65fc0deff7b785144b6c8fd5060e46407
Brett Andrews <brettandrews3@gmail.com>
2020-05-19 10:57:34 -0500

    Added IoC lines to Startup

--- IntermittentFasting/Startup.cs
+++ IntermittentFasting/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySql.Data.MySqlClient;
 
 namespace IntermittentFasting
 {
@@ -23,6 +25,16 @@ namespace IntermittentFasting
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //BRETT: Here's where I added in the IoC code:
+            services.AddScoped<IDbConnection>((s) =>
+            {
+                IDbConnection conn = new MySqlConnection(Configuration.GetConnectionString("intermittent_fasting_app"));
+                conn.Open();
+                return conn;
+            });
+
+            services.AddTransient<IFastingRepository>(); //Does a UserRepository come after IFR here?
+
             services.AddControllersWithViews();
         }
 
