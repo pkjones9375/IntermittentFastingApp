bac1a5ebab3cec1c9fc5284eed65fd99cad59bfc
Brett Andrews <brettandrews3@gmail.com>
2020-05-20 08:29:31 -0500

    Minor changes in controller, Index

--- IntermittentFasting/Controllers/UserController.cs
+++ IntermittentFasting/Controllers/UserController.cs
@@ -5,15 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+//BRETT: I'm writing the code for the UserController here, working from a similar example
+//in an ASP.NET exercise
 namespace IntermittentFasting.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IFastingRepository repo;
+
+        public UserController(IFastingRepository repo)
+        {
+            this.repo = repo;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            //BRETT: creating the var 'users' and calling 'GetAllUsers' here
+            //Remind Jeremy to write this method
+            var users = repo.GetAllUsers();
+
+            return View(users);
         }
     }
 }
--- IntermittentFasting/IIFastingRepository.cs
+++ IntermittentFasting/IIFastingRepository.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+// BRETT'S NOTE: I think that this segment might be superfluous. Delete later?
 namespace IntermittentFasting
 {
     interface IIFastingRepository
