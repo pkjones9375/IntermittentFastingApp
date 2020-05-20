0c328e28603b2e59d345f61b261e98663c5b5058
Brett Andrews <brettandrews3@gmail.com>
2020-05-20 09:35:57 -0500

    Renamed IFastingRepo to UserInfo

--- IntermittentFasting/UserRepository.cs
+++ IntermittentFasting/UserRepository.cs
@@ -3,12 +3,14 @@ namespace IntermittentFasting
 {
     //Here's the layout for our IF user info. Console will get/set BMI info
     //UserID will act as Primary Key in SQL
-    public class IFastingRepository
+    public class UserRepository
+        //*****Changed name of class to UserRepository, as it doesn't call the methods
+        //written by the IFastingRepository
     {
         public int UserID { get; set; }
         public string Name { get; set; }
-        public int Weight { get; set; }
         public int HeightInches { get; set; }
+        public int Weight { get; set; }
         public decimal StartBMI { get; set; }
         public decimal LastBMI { get; set; }
         public int TimeToFast { get; set; }
