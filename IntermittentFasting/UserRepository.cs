438720de2d0025eb419828bfe1376e330e4db2bd
Brett Andrews <brettandrews3@gmail.com>
2020-05-27 15:55:48 -0500

    Updates to user repos

--- IntermittentFasting/UserRepository.cs
+++ IntermittentFasting/UserRepository.cs
@@ -8,18 +8,13 @@ namespace IntermittentFasting
 {
     //Here's the layout for our IF user info. Console will get/set BMI info
     //UserID will act as Primary Key in SQL
-    public class UserRepository : IUserRepository
+    public class UserRepository : IUserRepository //B: Why does this have squiggles?
     {
-        private readonly IDbConnection _conn;
+        private readonly IDbConnection _conn; //B: Is this connected to the connection?
         
         public UserRepository(IDbConnection conn)
         {
             _conn = conn;
-        }
-
-        public void DeleteUser(User userToDelete)
-        {
-            throw new NotImplementedException();
         }
 
         public IEnumerable<User> GetAllUsers()
@@ -51,6 +46,17 @@ namespace IntermittentFasting
         {
             //***We need to figure out what parameters we want to update and how. How to update weight?
             //_conn.Execute("UPDATE users SET ")
+        }
+
+        //Implementing the DeleteUser() to remove users from database.
+        //Should connect to Models.User
+        public void DeleteUser(User user)
+        {
+            _conn.Execute("DELETE FROM users WHERE UserID = @id;",
+                new { id = user.UserID });
+
+            _conn.Execute("DELETE FROM weights WHERE UserID = @id;",
+                new { id = user.UserID });
         }
     }
 }
